4574fd7c5527d9bee0a0ede8a3cb3dc75fb42885
Julius426 <88243400+Julius426@users.noreply.github.com>
2021-10-03 18:49:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const prefix = "*"
+
+Client.on("ready", () => {
+console.log("bot opérationnel")
+});
+
+Client.login("ODkzNTczOTc3NzIzNzc3MDI0.YVdbng.JUJrWAeLj62fUbrkSyZe4OtC7Js")
+
+Client.on("messageCreate", message => {
+    if (message.author.bot) return;
+    
+    if(message.content === prefix + "ping"){
+        message.reply("pong !")
+    }
+
+    if(message.content === prefix + "site"){
+        message.reply("__**Voici notre site**__:\nhttps://lunardaydream796468631.wordpress.com/home/")}
+
+    else if(message.content === prefix + "help"){
+        message.channel.send("**__Les comandes du bot__**\n -__ping__ : renvoie pong\n -__site__ : le site de Lunar Daydream")
+    }
+
+
+});
+
+Client.login(process.env.TOKEN);
+
+
+
